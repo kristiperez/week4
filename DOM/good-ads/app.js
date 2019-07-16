9e00868af584283b9f4f1931261f77c7f0a3ef94
Kristi Perez <kristi.perezbedford@gmail.com>
2019-07-16 15:55:44 -0500

    task app

---
+++ DOM/good-ads/app.js
@@ -0,0 +1,7 @@
+
+// call the getRandomAd function
+
+// get the adImage by id
+let image = document.getElementById("adImage")
+// set the src property to the new random ad image  
+image.src = getRandomAd()
---
+++ DOM/todolist/app.js
@@ -0,0 +1,35 @@
+
+let addButton = document.getElementById("addButton")
+let taskTextBox = document.getElementById("taskTextBox")
+let pendingTasksUl = document.getElementById("pendingTasks")
+let completedTasksUl = document.getElementById("completedTasks")
+
+
+addButton.addEventListener('click',function() {
+
+    let task = taskTextBox.value
+    // create element
+    let pendingLi = document.createElement("li")
+    pendingLi.innerHTML = task // <li>Task1<li>
+
+    //create checkbox
+    let checkBox = document.createElement("input")
+    checkBox.type = "checkbox"
+    checkBox.addEventListener('click', function() {
+        if(this.checked) {completedTasksUl.appendChild(this.parentElement)
+        } else if(this.checked == false) {pendingTasksUl.appendChild(this.parentElement)}
+    })
+
+    // create remove button
+    let removeButton = document.createElement("button")
+    removeButton.innerHTML = "Remove"
+    removeButton.addEventListener('click', function() {
+        this.parentElement.parentElement.removeChild(this.parentElement)
+    })
+
+    pendingLi.appendChild(checkBox)
+    pendingLi.appendChild(removeButton)
+    pendingTasksUl.appendChild(pendingLi)
+    
+})
+
